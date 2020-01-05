19191f01a70c10454226514e8d81e155d494a863
mengruolin <18039596250@163.com>
2020-01-05 21:14:33 +0800

    Initial the page of project

---
+++ precache-manifest.113f6d736f3f7248b40aee8811f5bfe4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d63c0af3cf098240880cc3ea31108b69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "676452397c88674b0570",
+    "url": "/static/css/2.9204d6c0.chunk.css"
+  },
+  {
+    "revision": "ff24547da1b50be919e0",
+    "url": "/static/css/main.12a7c43c.chunk.css"
+  },
+  {
+    "revision": "676452397c88674b0570",
+    "url": "/static/js/2.33effab8.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/static/js/2.33effab8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff24547da1b50be919e0",
+    "url": "/static/js/main.c4b5cbe1.chunk.js"
+  },
+  {
+    "revision": "2cc0fefb3e5d0c25bb47",
+    "url": "/static/js/runtime-main.4ecdcb4c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
